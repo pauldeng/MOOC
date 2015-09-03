7d9964a2e88b77aec32dcc7065191da857c87b2a
Paul Deng <dengpeng.cn@gmail.com>
2015-09-03 10:27:48 +1000

    Done. Please compile and run the code under Linux and Oracle JDK 7.

--- Cloud Computing Applications/TopWords/MP1.java
+++ Cloud Computing Applications/TopWords/MP1.java
@@ -1,8 +1,9 @@
-import java.io.File;
+import java.io.*;
 import java.lang.reflect.Array;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
+import java.util.Comparator;
 
 public class MP1 {
     Random generator;
@@ -34,7 +35,7 @@ public class MP1 {
     }
 
     Integer[] getIndexes() throws NoSuchAlgorithmException {
-        Integer n = 10000;
+        Integer n = 10000; 
         Integer number_of_lines = 50000;
         Integer[] ret = new Integer[n];
         this.initialRandomGenerator(this.userName);
@@ -49,13 +50,66 @@ public class MP1 {
         this.inputFileName = inputFileName;
     }
 
+    public int indexOf(int[] array, int key) {
+        int returnvalue = -1;
+        for (int i = 0; i < array.length; ++i) {
+            if (key == array[i]) {
+                returnvalue = i;
+                break;
+            }
+        }
+        return returnvalue;
+    }
+
     public String[] process() throws Exception {
         String[] ret = new String[20];
-       
-        //TODO
+        int index = -1;
+        Integer[] indexes = getIndexes(); 
+
+        String[] lines = new String[50000];
+
+        try(BufferedReader br = new BufferedReader(new FileReader(inputFileName))) {
+
+            for(String line; (line = br.readLine()) != null; ) {
+                index += 1;
+                if (Arrays.asList(indexes).indexOf(index) == -1) continue;
+                lines[index] = line;
+            }            
+        }
+        SortedMap<String, Integer> tokens = new TreeMap<String, Integer>();
+
+        for (Integer i : indexes) {
+            StringTokenizer st = new StringTokenizer(lines[i], delimiters);
+            while (st.hasMoreElements()) {
+                String str = st.nextElement().toString().toLowerCase().trim();
+                if (Arrays.asList(stopWordsArray).indexOf(str) == -1){
+                    if (tokens.containsKey(str))
+                        tokens.put(str, tokens.get(str) + 1);
+                    else
+                        tokens.put(str, 1);
+                }
+            }
+        }
+
+        tokens = SortByValue(tokens); 
+
+        int count = 0;
+        for (Map.Entry<String, Integer> entry:tokens.entrySet()) {
+            if (count >= 20) break;
+
+            ret[count] = entry.getKey();
+            count++;
+        }
 
         return ret;
     }
+
+    public TreeMap<String, Integer> SortByValue(SortedMap<String, Integer> map) {
+		ValueComparator vc =  new ValueComparator(map);
+		TreeMap<String,Integer> sortedMap = new TreeMap<String,Integer>(vc);
+		sortedMap.putAll(map);
+		return sortedMap;
+	}
 
     public static void main(String[] args) throws Exception {
         if (args.length < 1){
@@ -70,5 +124,22 @@ public class MP1 {
                 System.out.println(item);
             }
         }
+    }
+}
+
+class ValueComparator implements Comparator<String> {
+ 
+    Map<String, Integer> map;
+ 
+    public ValueComparator(Map<String, Integer> base) {
+        this.map = base;
+    }
+ 
+    public int compare(String a, String b) {
+        if (map.get(a) > map.get(b)) {
+            return -1;
+        } else {
+            return 1;
+        } // returning 0 would merge keys 
     }
 }
